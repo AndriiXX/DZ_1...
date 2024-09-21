bebd1c202b423fa0c05117c131d7954099146114
AndriiXX <fugaandrii1@gmail.com>
2024-09-21 19:38:50 +0300

    Add project files.

---
+++ DZ_1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace DZ_1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+
+
+            app.MapGet("/", () => "Hello World!");
+
+            app.MapGet("/about", () => "ASP.NET Core");
+
+            //використовую Postman
+            app.MapPost("/echo", async (HttpContext context) =>
+            {
+                using var reader = new StreamReader(context.Request.Body);
+                var body = await reader.ReadToEndAsync();
+                return Results.Content(body, "application/json");
+            });
+
+            app.Use(async (context, next) =>
+            {
+                var start = DateTime.Now;
+                await next.Invoke();
+                var end = DateTime.Now;
+                Console.WriteLine($"Time request: {end - start}");
+            }); 
+
+            app.Run();
+        }
+    }
+}
